abee4e5b1e737c2cd7acaa7a8fef8f721fcbee0b
veronica365 <naruth10@gmail.com>
2023-03-08 18:08:55 +0300

    Add functionality to dynamically create a scores page

---
+++ src/modules/components.js
@@ -0,0 +1,26 @@
+export const addHeader = () => `<header><h1>Leaderboard</h1></header>`;
+
+export const addForm = () => `
+    <aside>
+        <h2>Add your score</h2>
+        <form>
+            <input placeholder="Your name"/>
+            <input placeholder="Your score" />
+            <button type="submit">Submit</button>
+        </form>
+    </aside>`;
+
+export const addMainSection = () => `
+    <main>
+        <section>
+            <div class="score-toolbar">
+                <h2>Recent scores</h2> <button>Refresh</button>
+            </div>
+            <div class="scores-wrapper"></div>
+        </section>
+    </main>
+`;
+
+export const addScore=(score)=>`
+    <article>Name: ${score}</article>
+`;
