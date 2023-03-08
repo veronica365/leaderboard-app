009f3edb3e749e38c25d9c6ec79d8e5ec05f8dc2
veronica365 <naruth10@gmail.com>
2023-03-08 18:08:55 +0300

    Add functionality to dynamically create a scores page

---
+++ src/modules/init-app.js
@@ -0,0 +1,29 @@
+import scores from "./scores.js";
+import { addHeader, addMainSection, addForm, addScore } from "./components.js";
+/**
+ * Create and insert default scores and an add score form
+ * @returns document Element
+ */
+const createScoresList = () => {
+    let innerHTML = "";
+    scores.forEach(score=>{
+        innerHTML += addScore(score);
+    })
+    return innerHTML;
+};
+
+/**
+ * Create and insert default scores and an add score form
+ * @returns document Element
+ */
+const appendHTML = () => {
+    return `<div class="wrapper">${addHeader()}${addMainSection()}${addForm()}</div>`;
+};
+/**
+ * Display initial scores list and add form event listeners
+ */
+const initialize = () => {
+    document.body.innerHTML = appendHTML();
+    document.querySelector(".scores-wrapper").innerHTML = createScoresList();
+};
+export default initialize;
