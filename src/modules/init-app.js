e63cb00e2e06f2ffa62aaa9f689fe0e1c43bc4e1
veronica365 <naruth10@gmail.com>
2023-03-09 07:18:26 +0300

    submit and refresh the page with results from the API

--- src/modules/init-app.js
+++ src/modules/init-app.js
@@ -6,10 +6,34 @@ import { addMainSection, addForm, addScore } from './components.js';
  */
 const createScoresList = () => {
   let innerHTML = '';
-  scores.forEach((score) => {
+  scores.scores.forEach((score) => {
     innerHTML += addScore(score);
   });
   return innerHTML;
+};
+
+/**
+ * Get all scores for a game created by you from the API
+ */
+const onRefresh = async () => {
+  await scores.getAllScores();
+  const currentScores = document.querySelector('.scores-wrapper');
+  currentScores.innerHTML = createScoresList();
+};
+
+/**
+ * Handle form submittion to add anew score
+ */
+const onSubmit = async (event) => {
+  event.preventDefault();
+  const form = document.querySelector('aside form');
+  const user = form.elements.name.value;
+  const score = parseInt(form.elements.score.value, 10);
+  await scores.AddNewScore({ user, score });
+  const currentScores = document.querySelector('.scores-wrapper');
+  currentScores.innerHTML = createScoresList();
+  form.elements.name.value = '';
+  form.elements.score.value = '';
 };
 
 /**
@@ -20,9 +44,13 @@ const appendHTML = () => `${addMainSection()}${addForm()}`;
 /**
  * Display initial scores list and add form event listeners
  */
-const initialize = () => {
-  const currentHTML = document.querySelector('.wrapper').innerHTML;
-  document.querySelector('.wrapper').innerHTML = `${currentHTML}${appendHTML()}`;
+const initialize = async () => {
+  const HTML = document.querySelector('.wrapper').innerHTML;
+  document.querySelector('.wrapper').innerHTML = `${HTML}${appendHTML()}`;
+  await scores.createGameName();
+  await scores.getAllScores();
   document.querySelector('.scores-wrapper').innerHTML = createScoresList();
+  document.querySelector('aside form').addEventListener('submit', onSubmit);
+  document.querySelector('.score-toolbar button').addEventListener('click', onRefresh);
 };
 export default initialize;
